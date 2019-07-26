e61068bb5d3fadde58b9d1d397cd0be0a8eebf65
kouguangyuan <2275738566@qq.com>
2019-07-26 17:59:36 +0800

    20190726

---
+++ 3基本库使用/urllib_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+__author__ = 'kgy'
+
+import urllib.request
+
+response = urllib.request.urlopen('http://c.biancheng.net/view/2406.html')
+
+print(response.read().decode('utf-8'))
+
+print(type(response))
+print(dir(response))
+# print(response.__all__)
+print([e for e in dir(response) if not e.startswith('_')])
+print(urllib.request.__file__)
+print(response.status)
+print(response.getheaders())
+print(response.getheader('Server'))
+
+print('*' * 30)
+import urllib.parse
+
+data = bytes(urllib.parse.urlencode({'word': 'hello'}), encoding='utf8')
+response = urllib.request.urlopen('http://httpbin.org/post', data=data)
+print(response.read().decode('utf-8'))
+
